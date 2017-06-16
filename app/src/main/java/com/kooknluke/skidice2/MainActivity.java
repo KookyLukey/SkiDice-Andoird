dce95d3a52e38b92c57b7103590da298e9a8d2e8
Luke O'Neill <lukeo3210@gmail.com>
2017-06-16 15:26:43 -0400

    Added slide bar for difficulty and added it to sharedpreferences

--- app/src/main/java/com/kooknluke/skidice2/MainActivity.java
+++ app/src/main/java/com/kooknluke/skidice2/MainActivity.java
@@ -2,12 +2,17 @@ package com.kooknluke.skidice2;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +20,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Context context = this;
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         Button settingsButton = (Button) findViewById(R.id.btnSettings);
+        Button testButton = (Button) findViewById(R.id.btnTest);
 
         assert settingsButton != null;
         settingsButton.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent settingsIntent =  new Intent(context, Settings.class);
                 startActivity(settingsIntent);
+            }
+        });
+
+        assert testButton != null;
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), Integer.toString(preferences.getInt("diffSetting", 0)), Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/kooknluke/skidice2/Settings.java
+++ app/src/main/java/com/kooknluke/skidice2/Settings.java
@@ -1,13 +1,46 @@
 package com.kooknluke.skidice2;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = preferences.edit();
+
+        final SeekBar difficultyBar = (SeekBar) findViewById(R.id.difficultyBar);
+
+        difficultyBar.setProgress(preferences.getInt("diffSetting", 0));
+
+        assert difficultyBar != null;
+        difficultyBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progressValue, boolean fromUser) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                editor.putInt("diffSetting", difficultyBar.getProgress());
+                editor.apply();
+            }
+        });
     }
 }
